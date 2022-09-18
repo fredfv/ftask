ab2615a77af76e59a2bcf3f8106f1a1730cf15c9
Fred Vieira <frederico.fv07@gmail.com>
2022-09-18 12:47:55 -0300

    Como utilizar transition

--- lib/src/modules/task/task_module.dart
+++ lib/src/modules/task/task_module.dart
@@ -2,15 +2,18 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:task/src/modules/task/views/list_task_page.dart';
 import 'package:task/src/modules/task/views/task_page.dart';
 
-class TaskModule extends Module{
+class TaskModule extends Module {
   @override
   List<Bind> get binds => [];
 
   @override
   List<ModularRoute> get routes => [
-      ChildRoute('/', child: (_, __) => const TaskPage()),
+        ChildRoute('/', child: (_, __) => const TaskPage()),
         //ChildRoute('/list/:id', child: (_, args) => ListTaskPage(id : args.params['id'] ?? ''))
         //ChildRoute('/list', child: (_, args) => ListTaskPage(id : args.queryParams['id'] ?? ''))
-        ChildRoute('/list', child: (_, args) => ListTaskPage(id : args.data ?? ''))
+        ChildRoute('/list',
+            child: (_, args) => ListTaskPage(id: args.data ?? ''),
+            transition: TransitionType.fadeIn,
+            duration: const Duration(milliseconds: 500))
       ];
 }
