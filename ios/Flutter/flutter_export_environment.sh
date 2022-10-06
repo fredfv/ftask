2edd6f26040f9274cba99ffafbcf39e5be0745ef
fred <frederico.fv@hotmail.com>
2022-10-06 17:10:45 -0300

    git ignore

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/fred/src/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/fred/devenv/task"
+export "FLUTTER_APPLICATION_PATH=/Users/fred/devenv/ftask"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/fred/devenv/task/lib/main.dart"
+export "FLUTTER_TARGET=/Users/fred/devenv/ftask/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVF
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/fred/devenv/task/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/fred/devenv/ftask/.dart_tool/package_config.json"
